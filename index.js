00cbbc404a76097f29824cfd6e6c50776b3bbcd6
Koki Ito <kokiito0926@gmail.com>
2026-02-28 18:14:30 +0900

    fix: 🐛 styleのタグを解析するときにエラーが発生していた
    そのため、sanitize-htmlを使用して、styleのタグを削除するようにした。
    それから、JSDOMの仮想コンソールを用いて、エラーと警告をスキップするようにした。

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+// 下記のコマンドを実行すると、エラーが発生してしまう。
+// styleのタグを解析するときに問題が発生していたので、styleのタグは削除するようにした。
+// >> $ curl -fsSL https://developers.cloudflare.com/email-routing/email-workers/send-email-workers/ | ./index.js
+/*
+TypeError: Cannot create property 'border-width' on string 'unset'
+*/
+// >> 2026/02/28 18:13.
+
 // 下記のようにスクリプトを実行すれば、stdinの問題は発生しない。
 // >> $ curl https://example.com/ | ./src/example.js
 // >> $ curl https://example.com/ | zx ./src/example.js
@@ -19,7 +27,8 @@ stdin is not a tty
 // >> 2026/01/26 18:27.
 
 import { stdin } from "zx";
-import { JSDOM } from "jsdom";
+import sanitizeHtml from "sanitize-html";
+import { JSDOM, VirtualConsole } from "jsdom";
 import { Readability } from "@mozilla/readability";
 
 if (process.stdin.isTTY) {
@@ -31,9 +40,31 @@ if (!html) {
 	process.exit(1);
 }
 
-const dom = new JSDOM(html);
+const cleanHtml = sanitizeHtml(html, {
+	nonTextTags: ["style", "script", "noscript"],
+});
 
-const reader = new Readability(dom.window.document);
+const virtualConsole = new VirtualConsole();
+virtualConsole.on("error", () => {});
+virtualConsole.on("warn", () => {});
+
+const dom = new JSDOM(cleanHtml, {
+	pretendToBeVisual: false,
+	resources: "usable",
+	runScripts: "outside-only",
+
+	features: {
+		FetchExternalResources: false,
+		ProcessExternalresources: false,
+	},
+
+	virtualConsole: virtualConsole,
+});
+
+const reader = new Readability(dom.window.document, {
+	keepClasses: true,
+});
+
 const article = reader.parse();
 if (!article) {
 	process.exit(1);
